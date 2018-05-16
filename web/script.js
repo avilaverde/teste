e01c55dc13319360a06f2e057c7b8a082a5ff3d4
André Vila Verde <andre@asus.lan>
2018-05-17 00:52:35 +0100

    teste de git

---
+++ web/script.js
@@ -0,0 +1,7 @@
+function teste() {
+	var preco=12.50, quantidade=12, total= preco*quantidade;
+	document.getElementById('info').innerHTML = total;
+	// document.getElementById("demo").innerHTML = "Paragraph changed!";
+
+}
+function 
